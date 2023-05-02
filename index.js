48634a5765199c36f7f14954013e6a5098b41bf1
YusufHasan94 <yhpolok@gmail.com>
2023-05-03 00:36:27 +0600

    ready chefs data for client site

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(cors());
 const port = process.env.PORT || 5000;
+
+const chef= require('./data/chefs.json');
 
 app.get('/', (req, res)=>{
     res.send('Hello!');
 })
-
+app.get('/chefs',(req, res)=>{
+    res.send(chef);
+})
 app.listen(port, ()=>{
     console.log(`Port ${port} running successfully`);
 })
